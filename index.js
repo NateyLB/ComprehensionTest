e62c518aab3ec146548b2fb4f64cc7e71ade059e
Nathan <nateyl.b@gmail.com>
2020-03-30 19:01:22 -0700

    finished

--- index.js
+++ index.js
@@ -5,7 +5,9 @@ console.log("working");
 //Write a function "min" that takes 2 arguements which should be numbers and returns to lowest of the two 
 //Code Under this line
 
-
+function min (num1, num2){
+    return Math.min(num1, num2);
+}
 
 
 
@@ -13,10 +15,19 @@ console.log("working");
 /*Write a function "range" which takes two arguments "start" and "end" that returns an array containing all of the 
 integers between and including start and end in sequential order */
 //code under this line 
+function range(start, end){
+    var arr = [];
+    for(var i=start; i<=end; i++){
+    arr.push(i)
+    }
+    return arr;
+}
 
 
-
-/*Write a function 'sum' which takes an Array of numbers as an  and argument and returns their sum
+/*Write a function 'sum' which takes an Array of numbers as an  and argument and returns their sum*/
+function sum(num1, num2){
+    return num1 + num2;
+}
 
 
 
@@ -28,7 +39,21 @@ For this problem consider researching the modulus operator */
 
 function fizzBuzz(){
     //CODE HERE
+    const arr = range(1,100);
+    arr.forEach(element =>{
+        if(element%3 == 0){
+            console.log("BUZZ");
+        }
+        else if(element%5 == 0){
+            console.log("FIZZ")
+        }
+        else{
+            console.log(element)
+        }
+    })
 }
+
+fizzBuzz();
 
 /* You can get the Nth character (letter) from a string by writting the string variable name plus [N] e.g:
 
@@ -43,13 +68,25 @@ write another function which extends the first function by taking in a second ar
 to count the number of.  */
 
 function countBs (string){
-//CODE HERE
+    for(var i = 0, len = string.length, count=0, ch; i < len; ++i)
+    {
+        ch = string.charAt(i);
+        if(ch =='B') ++count;
+    }
+    return count
 }
 
 function countChars (string, character){
-//CODE HERE
+    for(var i = 0, len = string.length, count=0, ch; i < len; ++i)
+    {
+        ch = string.charAt(i);
+        if(ch == character) ++count;
+    }
+    return count
 }
 
+console.log(countBs("Bello BaBy I baby"));
+console.log(countChars("Eveyrythin EEE E ee E", 'E'));
 /* Write a loop that makes seven calls to console.log to output the following triangle:
 
 ________________________________
@@ -68,8 +105,13 @@ eg. let abc = 'abc';
 
 function sevenLoop () {
     //CODE HERE
-    
+    var string = ''
+    for(var i=0; i<7; i++){
+    string += '#';
+    console.log(string)
     }
+    }
+    sevenLoop();
 
 /* Write a program that creates a string which represents an 8x8 grid. At each posistion of the grid  there is either
 a space or a # character the characters should form a "cheesboard" when console.log'ed something like this should be 
@@ -91,9 +133,13 @@ __________________________________________
 once you have completed the program take in an arguement that varies the size of the grid */
 
 function chessBoard (gridSize){
-//CODE HERE
+const black=" # # # #";
+const white="# # # #";
+for(var i = 0; i<4+gridSize; i++){
+    console.log(black);
+    console.log(white);
 }
-
+}
 
 
 //ANY ADDITIONAL COMMENTS OR CONCERNS CAN BE LEFT UNDER THIS LINE 
